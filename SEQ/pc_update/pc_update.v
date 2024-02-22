3fce76c76f5d22595879c160fcd4b81254411631
Janya Gupta <129752072+JanyaGupta05@users.noreply.github.com>
2024-02-22 22:08:02 +0530

    Add files via upload

--- SEQ/pc_update/pc_update.v
+++ SEQ/pc_update/pc_update.v
@@ -1,12 +1,16 @@
 module pc_update(input wire clk, input cnd, input [3:0] icode, input [63:0] ValP, input [63:0] ValC, input [63:0] ValM, output reg [63:0] newPc);
 
+
+initial begin
+newPc=0;
+end
 always @(*)
 begin
-    if (icode==4'b0000)//halt
-    begin
-        newPc = 0;
-    end
-    else if (icode==4'b0111)//jump
+    // if (icode==4'b0000)//halt
+    // begin
+    //     newPc = 0;
+    // end
+    if (icode==4'b0111)//jump
     begin
         if (cnd==1)
         begin
